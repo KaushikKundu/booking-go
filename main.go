b0f73915cd143d04b30a1ae03aa86b74241bf1b6
Kaushik kundu <kaushikkundu774@gmail.com>
2025-07-02 11:27:48 +0530

    refactor

--- main.go
+++ main.go
@@ -6,42 +6,57 @@ import (
 	"strings"
 )
 
+const conferenceTickets int = 50
+
+var remainingTickets uint = 50
+var bookings []string
+
+type User struct {
+}
+
 func main() {
-	var conferenceName = "Go Conference"
-	const conferenceTickets int = 50
-	var remainingTickets uint = 50
-	var bookings []string
 
-	fmt.Println("Welcome to", conferenceName, "booking application")
-	for {
-		firstName,lastName,email,userTickets := getUserInput()
-		isUserDataValid := validateUserInput(firstName, lastName, email, userTickets)
-		if isUserDataValid {
-			if userTickets > remainingTickets {
-				fmt.Printf("Sorry, we only have %d tickets remaining.\n", remainingTickets)
-			}
-			remainingTickets -= userTickets
-			bookings = append(bookings, firstName+" "+lastName)
-
-			fmt.Printf("Thanks you for booking %v tickets, You will receive mail shortly!\n", userTickets)
+	greetUsers()
+	for remainingTickets > 0 {
+		firstName, lastName, email, userTickets := getUserInput()
+		isValidFirstName, isValidLastName, isValidEmail, isTicketValid := validateUserInput(firstName, lastName, email, userTickets)
+		if !isValidFirstName || !isValidLastName {
+			fmt.Println("Names invalid  or too short.")
+			continue
 		}
-
-		firstnames := []string{}
-		for _, booking := range bookings {
-			var names = strings.Fields(booking)
-			firstnames = append(firstnames, names[0])
+		if !isTicketValid {
+			fmt.Println("We don't have that much tickets. tickets left:",remainingTickets)
+			continue
 		}
+		if !isValidEmail {
+			fmt.Println("Invalid email")
+			continue
+		}
+		bookTickets(userTickets)
+		printNames(bookings)
+
 	}
 
 }
-func validateUserInput(firstName, lastName, email string, tickets uint) bool {
+func bookTickets(userTickets uint) {
+	remainingTickets -= userTickets
+	fmt.Println("tickets left:", remainingTickets)
+}
+func printNames(bookings []string) {
+	firstnames := []string{}
+	for _, booking := range bookings {
+		var names = strings.Fields(booking)
+		firstnames = append(firstnames, names[0])
+	}
+}
+func validateUserInput(firstName, lastName, email string, tickets uint) (bool, bool, bool, bool) {
 	isValidFirstName := len(strings.TrimSpace(firstName)) > 0 && reflect.TypeOf(firstName) == reflect.TypeOf("")
 	isValidLastName := len(strings.TrimSpace(lastName)) > 0 && reflect.TypeOf(lastName) == reflect.TypeOf("")
 	isValidEmail := len(strings.TrimSpace(email)) > 0 && reflect.TypeOf(email) == reflect.TypeOf("")
-	isTicketValid := tickets > 0
-	return isTicketValid && isValidEmail && isValidFirstName && isValidLastName
+	isTicketValid := tickets > 0 && tickets <= remainingTickets
+	return isValidFirstName, isValidLastName, isValidEmail, isTicketValid
 }
-func getUserInput()(string, string, string, uint ) {
+func getUserInput() (string, string, string, uint) {
 	var firstName string
 	var lastName string
 	var email string
@@ -55,5 +70,9 @@ func getUserInput()(string, string, string, uint ) {
 	fmt.Scan(&email)
 	fmt.Println("Enter number of tickets you want to book:")
 	fmt.Scan(&userTickets)
-	return firstName,lastName,email,userTickets
+	return firstName, lastName, email, userTickets
+}
+func greetUsers() {
+	var conferenceName = "Go Conference"
+	fmt.Println("Welcome to", conferenceName, "booking application")
 }
